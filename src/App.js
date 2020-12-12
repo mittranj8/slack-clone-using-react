3901894f731279ef8c5332711533f51b72d54ed9
Mittranj <mpansuriya@unomaha.edu>
2020-12-11 22:33:58 -0600

    added sidebar to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import Header from "./Header";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Sidebar */}
+      <div className="app_body">
+        <Sidebar />
+      </div>
+
       {/* React Router */}
     </div>
   );
