842526e5c4bf0a6219a65aec15b4963fab830ed9
Mittranj <mpansuriya@unomaha.edu>
2020-12-13 18:27:09 -0600

    added initial routes for the app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -5,12 +6,22 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app_body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app_body">
+          <Sidebar />
 
-      {/* React Router */}
+          <Switch>
+            <Route path="/room/:roomId">
+              {/* <Chat /> */}
+              <h1>Chat Screen</h1>
+            </Route>
+            <Route path="/">
+              <h1>Welcome to Slack App</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
