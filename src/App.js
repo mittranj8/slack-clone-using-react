9d66493215a3199771ef0d57465b9724428d6c93
Mittranj <mpansuriya@unomaha.edu>
2020-12-14 20:29:20 -0600

    added state provider to app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Chat from "./components/Chat";
 import Header from "./components/Header";
+import Login from "./components/Login";
 import Sidebar from "./components/Sidebar";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  // const [user, setUser] = useState(null);
+  const [{user}, dispatch] = useStateValue();
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app_body">
-          <Sidebar />
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app_body">
+              <Sidebar />
 
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome to Slack App</h1>
-            </Route>
-          </Switch>
-        </div>
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome to Slack App</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
